a1e72819642791c7ddeb1147c4b7e62f50d99bd1
Simón Gotta <simon.gotta@gmail.com>
2019-05-25 16:31:38 -0300

    Add yarn for dependency management and material-ui for handling the interface

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,20 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  // Link,
-  Route,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import StartScreen from './pages/StartScreen';
 import './App.css';
 
 
 function App() {
   return (
-    <Router>
-      <Route exact path="/" component={StartScreen} />
-      {/* <Route exact path="/game-screen" component={GameScreen} /> */}
-      {/* <Route exact path="/end-game-screen" component={EndGameScreen} /> */}
-    </Router>
+    <MuiThemeProvider>
+      <Router>
+        <Route exact path="/" component={StartScreen} />
+        {/* <Route exact path="/game-screen" component={GameScreen} /> */}
+        {/* <Route exact path="/end-game-screen" component={EndGameScreen} /> */}
+      </Router>
+    </MuiThemeProvider>
   );
 }
 
